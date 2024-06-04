4aba19ed4bf55fd98f590141dcedc6c17ea8b92c
plyn <plyn_rider@outlook.com>
2024-06-04 03:32:38 -0700

    'updated'

--- controllers/product.js
+++ controllers/product.js
@@ -32,7 +32,6 @@ exports.create = (req, res) => {
         error: 'Image could not be uploaded',
       });
     }
-    // check for all fields
     const { name, description, price, category, quantity, shipping } = fields;
 
     if (
@@ -50,11 +49,7 @@ exports.create = (req, res) => {
 
     let product = new Product(fields);
 
-    // 1kb = 1000
-    // 1mb = 1000000
-
     if (files.photo) {
-      // console.log("FILES PHOTO: ", files.photo);
       if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: 'Image should be less than 1mb in size',
@@ -103,11 +98,7 @@ exports.update = (req, res) => {
     let product = req.product;
     product = _.extend(product, fields);
 
-    // 1kb = 1000
-    // 1mb = 1000000
-
     if (files.photo) {
-      // console.log("FILES PHOTO: ", files.photo);
       if (files.photo.size > 1000000) {
         return res.status(400).json({
           error: 'Image should be less than 1mb in size',
@@ -128,13 +119,6 @@ exports.update = (req, res) => {
   });
 };
 
-/**
- * sell / arrival
- * by sell = /products?sortBy=sold&order=desc&limit=4
- * by arrival = /products?sortBy=createdAt&order=desc&limit=4
- * if no params are sent, then all products are returned
- */
-
 exports.list = (req, res) => {
   let order = req.query.order ? req.query.order : 'asc';
   let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
@@ -154,11 +138,6 @@ exports.list = (req, res) => {
       res.json(products);
     });
 };
-
-/**
- * it will find the products based on the req product category
- * other products that has the same category, will be returned
- */
 
 exports.listRelated = (req, res) => {
   let limit = req.query.limit ? parseInt(req.query.limit) : 6;
@@ -187,14 +166,6 @@ exports.listCategories = (req, res) => {
   });
 };
 
-/**
- * list products by search
- * we will implement product search in react frontend
- * we will show categories in checkbox and price range in radio buttons
- * as the user clicks on those checkbox and radio buttons
- * we will make api request and show the products to users based on what he wants
- */
-
 exports.listBySearch = (req, res) => {
   let order = req.body.order ? req.body.order : 'desc';
   let sortBy = req.body.sortBy ? req.body.sortBy : '_id';
@@ -202,14 +173,10 @@ exports.listBySearch = (req, res) => {
   let skip = parseInt(req.body.skip);
   let findArgs = {};
 
-  // console.log(order, sortBy, limit, skip, req.body.filters);
-  // console.log("findArgs", findArgs);
 
   for (let key in req.body.filters) {
     if (req.body.filters[key].length > 0) {
       if (key === 'price') {
-        // gte -  greater than price [0-10]
-        // lte - less than
         findArgs[key] = {
           $gte: req.body.filters[key][0],
           $lte: req.body.filters[key][1],
@@ -248,17 +215,12 @@ exports.photo = (req, res, next) => {
 };
 
 exports.listSearch = (req, res) => {
-  // create query object to hold search value and category value
   const query = {};
-  // assign search value to query.name
   if (req.query.search) {
     query.name = { $regex: req.query.search, $options: 'i' };
-    // assigne category value to query.category
     if (req.query.category && req.query.category != 'All') {
       query.category = req.query.category;
     }
-    // find the product based on query object with 2 properties
-    // search and category
     Product.find(query, (err, products) => {
       if (err) {
         return res.status(400).json({
